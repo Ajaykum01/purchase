58946567f43dc15495df69b22f800c4031c3be2e
Ajaykum01 <115863768+Ajaykum01@users.noreply.github.com>
2023-11-23 12:17:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -169,8 +169,8 @@ If You Still Don't Get Movies then Owner Will Update Within 12Hour"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-
-    
+    CAPTION = """
+<b>{file_name}</b> \n\n🏆 <i><b>Main Channel » [Leothalapathy](https://t.me/+jARGKkERm1RlNDU1)</i>"""
     IMDB_TEMPLATE_TXT = """
 <b><i>💫 Title : <a href={url}>{title}</a>  ({year}) 🎭 Genres : {genres} 🌟 IMDB Rating: <a href={url}/ratings>{rating}</a> / 10   📌 [How To Download](https://t.me/kpslinkteam/13) </i></b>"""
     
